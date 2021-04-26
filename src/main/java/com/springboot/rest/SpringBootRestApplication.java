28870130f9edb6ebf4a7d48c1312453bec91112f
Vishal <vishal007gohan@gmail.com>
2021-04-26 14:52:02 +0530

    First Commit

---
+++ src/main/java/com/springboot/rest/SpringBootRestApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+	public static void main(String ar[]) {
+		SpringApplication.run(SpringBootRestApplication.class, ar);
+	}
+}
